1d0ffef302af203ecf16669190796e3c3a9cf087
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-01-24 14:21:52 -0300

    Operadores feito

--- php_ti97/index.php
+++ php_ti97/index.php
@@ -1,3 +1,9 @@
+<? 
+$inteiro = 100;
+$decimal = (double) $inteiro;
+var_dump($inteiro);
+var_dump($decimal);
+?>
 
 <?php 
 $num1 = 10;
@@ -13,4 +19,12 @@ echo "subtração: $subtração<br>";
 echo "Multiplicação: $multiplicação<br>";
 echo "Divisão: $divisão<br>";
 
+// Operadores
+
+$resultado = $num1 + $num2; //adição
+$resultado = $num1 - $num2; //subtração
+$resultado = $num1 * $num2; //multiplicação
+$resultado = $num1 / $num2; //divisão
+$resultado = $num1 % $num2; //resto
+$resultado = $num1 ** $num2; //potenciação
 ?>
