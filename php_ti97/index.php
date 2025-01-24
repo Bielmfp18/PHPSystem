47e4ebd87ab06110a7e860eac4735fe66ae25198
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-01-24 14:19:23 -0300

    Código de Sinais Matemáticos

--- php_ti97/index.php
+++ php_ti97/index.php
@@ -1,6 +1,16 @@
-<? 
-$inteiro = 100;
-$decimal = (double) $inteiro;
-var_dump($inteiro);
-var_dump($decimal);
+
+<?php 
+$num1 = 10;
+$num2 = 5;
+
+$adição = $num1 + $num2;
+$subtração = $num1 - $num2;
+$multiplicação = $num1 * $num2;
+$divisão = $num1 / $num2;
+
+echo "adição: $adição<br>";
+echo "subtração: $subtração<br>";
+echo "Multiplicação: $multiplicação<br>";
+echo "Divisão: $divisão<br>";
+
 ?>
