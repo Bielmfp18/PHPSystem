23eec7e4a56e3354f48e0322a1673b4fc980c41c
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-01-31 17:06:15 -0300

    Atividade de cadastro feito

--- php_ti97/PROJETO_PHP/validar.php
+++ php_ti97/PROJETO_PHP/validar.php
@@ -11,16 +11,38 @@
 
 //SUPER GLOBAL para login (Array)//
 //Atividade Feita =)
+// $usuario = array(
+//     array('email'=> 'admin@email.com', 'senha' => '123456'),
+//     array('email'=> 'usuario@email.com', 'senha' => '123456')
+// );
+
+// foreach($usuario as $user ){
+//  echo 'Usuário array:'.$user['email'].'/'.$user['senha'];
+//  echo '<br>';
+//  echo 'Usuário form:'.$_POST['email'].'/'.$_POST['senha'];
+//  echo '<hr>';
+// }
+
+////////////////////////////////////////////////////////////////
+
+//Variável que verifica se a autentificação foi feita
+$UsuarioAutenticado = false;
+//Usuarios do Sistema 
 $usuario = array(
-    array('email'=> 'admin@email.com', 'senha' => '123456'),
-    array('email'=> 'usuario@senac.com', 'senha' => '123'),
+    array('email'=> 'admin@teste.com', 'senha' => '123456'),
+    array('email'=> 'usuario@teste.com', 'senha' => '123456')
 );
 
-foreach($usuario as $user ){
- echo 'Usuário array:'.$user['email'].'/'.$user['senha'];
- echo '<br>';
- echo 'Usuário array:'.$_POST['email'].'/'.$_POST['senha'];
- echo '<hr>';
+foreach($usuario as $user){
+if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+    $UsuarioAutenticado = true;
+}
+}
+if($UsuarioAutenticado){
+echo 'Usuário autenticado';
+}else {
+
+header('Location: index.php? login=erro em logar');
 }
 
 
