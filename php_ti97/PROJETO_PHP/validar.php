f751d34062c9ef0c1607b0da24896ee6439ede56
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-01-31 15:57:34 -0300

    Atividade de chave array ($_POST) feita e corrigida

--- php_ti97/PROJETO_PHP/validar.php
+++ php_ti97/PROJETO_PHP/validar.php
@@ -2,10 +2,27 @@
 //SUPER GLOBAL//
 
 // echo "Olá formulário";
-print_r($_POST);//Super Global recebe os valores como uma array.
-echo '<br>';//Super Globais são sempre acompanhadas do ($) e todas as letras são maiúsculas.
-echo $_POST['email'];
-echo '<br>';
-echo $_POST['senha'];
+
+// print_r($_POST);//Super Global recebe os valores como uma array.
+// echo '<br>';//Super Globais são sempre acompanhadas do ($) e todas as letras são maiúsculas.
+// echo $_POST['email'];
+// echo '<br>';
+// echo $_POST['senha'];
+
+//SUPER GLOBAL para login (Array)//
+//Atividade Feita =)
+$usuario = array(
+    array('email'=> 'admin@email.com', 'senha' => '123456'),
+    array('email'=> 'usuario@senac.com', 'senha' => '123'),
+);
+
+foreach($usuario as $user ){
+ echo 'Usuário array:'.$user['email'].'/'.$user['senha'];
+ echo '<br>';
+ echo 'Usuário array:'.$_POST['email'].'/'.$_POST['senha'];
+ echo '<hr>';
+}
+
+
 
 ?>
