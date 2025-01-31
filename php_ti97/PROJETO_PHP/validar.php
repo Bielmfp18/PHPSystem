bf853de6aa5558400bf8a2a987169b9b185ca48a
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-01-31 15:06:18 -0300

    Super global ($_POST) feito

--- php_ti97/PROJETO_PHP/validar.php
+++ php_ti97/PROJETO_PHP/validar.php
@@ -1,5 +1,11 @@
 <?php 
+//SUPER GLOBAL//
+
 // echo "Olá formulário";
-print_r($_POST)//Super Global recebe os valores como uma array.
-                //Super Globais são sempre acompanhadas do ($) e todas as letras são maiúsculas.
+print_r($_POST);//Super Global recebe os valores como uma array.
+echo '<br>';//Super Globais são sempre acompanhadas do ($) e todas as letras são maiúsculas.
+echo $_POST['email'];
+echo '<br>';
+echo $_POST['senha'];
+
 ?>
