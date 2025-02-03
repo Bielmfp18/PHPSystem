8cda6676273e138fdba113a4c4753fe21ce9cd97
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-03 14:40:34 -0300

    Conexão com o Banco de Dados Através do PHP concluída

--- PHP_BD/conecta.php
+++ PHP_BD/conecta.php
@@ -1,14 +1,21 @@
-<?php 
+<?php
 
 //Parâmetros para a conexão com o Banco de Dados
 
-$conn = 'mysql:host=localhost; bd_ti97';
+$bd = 'mysql:host=localhost; bd_ti97';
 $usuario = 'root';
 $senha = '';
 
 //Conexão com o Banco de Dados
 
-//$conn = $bd.$usuario.$senha;
-
-$conn = new PDO($bd.$usuario.$senha) //PDO -> É uma classe que está dispnível no PHP para a conexão com o PHP.
-?>
+//$conn = $bd,$usuario,$senha;
+try {
+    $conn = new PDO($bd, $usuario, $senha);
+    echo "BD ok"; //PDO -> É uma classe que está dispnível no PHP para a conexão com o PHP.
+} catch (PDOException $e) {
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    echo "Erro no BD (Conexão)";
+}
+// Tratamento de excessão (PDOException) -> Retorna uma ação não desejada (Erro) do Sistema ao Usuário.
