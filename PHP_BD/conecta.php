c62ec2374bf696a80449b65a5078f2e2b25d6788
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-03 14:15:20 -0300

    Adicionando um novo PDO para conexão com o Banco de Dados

--- PHP_BD/conecta.php
+++ PHP_BD/conecta.php
@@ -1,5 +1,14 @@
 <?php 
+
+//Parâmetros para a conexão com o Banco de Dados
+
 $conn = 'mysql:host=localhost; bd_ti97';
 $usuario = 'root';
 $senha = '';
+
+//Conexão com o Banco de Dados
+
+//$conn = $bd.$usuario.$senha;
+
+$conn = new PDO($bd.$usuario.$senha) //PDO -> É uma classe que está dispnível no PHP para a conexão com o PHP.
 ?>
