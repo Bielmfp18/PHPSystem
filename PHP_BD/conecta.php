c0b9d50c21c687c379592b8d8c4af71e10a7bc73
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-03 16:27:51 -0300

    Nova atividade

--- PHP_BD/conecta.php
+++ PHP_BD/conecta.php
@@ -3,20 +3,28 @@
 //Parâmetros para a conexão com o Banco de Dados
 
 $bd = 'mysql:host=localhost; bd_ti97';
-$usuario = 'roott';
+$usuario = 'root';
 $senha = '';
 
 //Conexão com o Banco de Dados
 
 //$conn = $bd,$usuario,$senha;
 try {
-    $conn = new PDO($bd, $usuario, $senha);
-    echo "BD ok"; //PDO -> É uma classe que está dispnível no PHP para a conexão com o PHP.
-
-
-} 
+    $conn = new PDO($bd, $usuario, $senha); //PDO -> É uma classe que está dispnível no PHP para a conexão com o PHP.
+    $query = '
+    Use bd_ti97;
+    create table usuarios2(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(32) NOT NULL);
+    ';
+// echo "BD ok";
+$retorno = $conn->exec($query); //exec -> Executar o que estiver dentro dos parênteses;
+echo $retorno;
+}
+     
 // Tratamento de excessão (PDOException) -> Retorna uma ação não desejada (Erro) do Sistema ao Usuário.
-
 catch (PDOException $e) {
 echo "Erro DB".$e->getMessage();
 }
