6fae6ac2de33f91de6ab2391a63974390de066c5
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-05 14:30:02 -0300

    Verificando a conexão

--- PHP_session/conexão.php
+++ PHP_session/conexão.php
@@ -8,4 +8,8 @@ $database = "session";
 //Criando conexão
 
 $conn = new mysqli($servername, $username, $password, $database);
+// Verificando a conexão
+if($conn->connect_error){
+    die("Falha na conexão:" .$conn->connect_error);
+}
 ?>
