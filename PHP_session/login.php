90d52c1e109592efc53f60f690493a259455606c
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-05 15:36:53 -0300

    Verifica se encontrou algum registro

--- PHP_session/login.php
+++ PHP_session/login.php
@@ -1,6 +1,23 @@
 <?php 
 
-//Criar session
+//Criar sessão
+session_start();
+//Incluir o arquivo de conexão
+include 'conexao.php';
+//verificar se os dados foram enviados via POST
+if(isset ($_POST['usuario'])&& isset($_POST['senha'])){
+    //Recebe dados do form
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
 
+    //Monta a consulta SQL
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+$result = $conn->query($sql);
+//Verifica se encontrou algum registro
+if($result && $result->num_rows>0){
+    $_SESSION['usuario']=$usuario;
+    echo"Loginrealizado com sucesso!";
+}
+}
 
 ?>
