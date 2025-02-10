540c8fedbd66bfbd4d0ef24f7802b91cb395e343
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-10 13:51:49 -0300

    Iniciando um novo projeto

--- PHP_index/index.php
+++ PHP_index/index.php
@@ -1,3 +1,8 @@
 <?php 
+// include('teste.php');
+// require('teste.php');
+require_once('teste.php'); //require_once solicita o texto do arquivo teste.php sem atualiza-lo
+echo "Olá! eu sou teste.php";
+echo "<br>";
 
 ?>
---
+++ php_ti97/Projeto_date/Date.php
@@ -0,0 +1,8 @@
+<?php
+//Date
+// echo date("d/m/Y H:i:s");
+// $data = date("d/m/Y H:i:s");
+
+//Timestamp
+$timestamp = strtotime("2025-01-10");
+echo date("d/m/Y", $timestamp);?>
