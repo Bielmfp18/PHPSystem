98628fc75ef8fe6b2aa83739139d94f81e23cc2d
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-12 14:23:14 -0300

    Comentários adicionados

--- PHP_new/conecta.php
+++ PHP_new/conecta.php
@@ -1,4 +1,8 @@
 <?php
+
+//Include inclui os dados de um arquivo em outro.
+//Require requere os dados de um outro arquivo para "rodar".
+
 //O try é utilizado para previnir todo tipo de erro. 
 try{
 //O PDO pode ser usado para se conectar a qualquer tipo de Banco de Dados.
@@ -10,12 +14,16 @@ echo "Banco de Dados ok";
 }
 
 //PDOException para erros no PDO
-// catch(PDOException){
-
-// }
+catch(PDOException $e){
+echo "Erro no PDO ".$e->getMessage();
+}
 
 //Exception para erros comuns/genéricos.
 catch(Exception $e){
     echo "Erro no Banco de Dados ".$e->getMessage();
 }
+
+
+
+
 ?>
