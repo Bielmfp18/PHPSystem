f2810e6763c1de0cef3eaefadfc7a21681b15758
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-14 14:01:27 -0300

    Terminando o arquivo config.php

---
+++ PHP_CRUD/config.php
@@ -0,0 +1,31 @@
+<?php 
+// Define o endereço do servidor de banco de dados (neste caso, o servidor local)
+
+$host= "localhost";
+
+// Define o nome do banco de dados que será utilizado
+
+$dbname = "crud";
+
+// Define o nome de usuário para conexão com o banco de dados
+
+$username = "root";
+
+// Define a senha para conexão com o banco de dados (vazia neste exemplo)
+
+$password = "";
+
+// Inicia um bloco try-catch para tentar estabelecer a conexão e tratar possíveis erros
+
+try {
+    // Cria uma nova instância do PDO para conectar ao banco de dados MySQL
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname,$username ,$password");
+}catch(PDOException $e){
+
+    // Interrompe a execução do script e exibe uma mensagem de erro com detalhes
+ 
+die("Erro no Banco de Dados ".$e->getMessage());
+}
+
+?>
