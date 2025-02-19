d7aaa5b53fab055cd4631fb150b7c6e557c4eb9e
Bielmfp18 <gabrielmartins180706@gmail.com>
2025-02-19 14:02:52 -0300

    Projeto Global iniciado

---
+++ PHP_GLOBAL/index.php
@@ -0,0 +1,8 @@
+<?php   
+//$_SESSION:
+/*Utilizada para gerenciar sessões. Permite armazenar dados que persistem durante a navegação do usuário no site.
+para usa-lá, é necessário iniciar a sessão com session_start():*/
+
+
+
+?>
